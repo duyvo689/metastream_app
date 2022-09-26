74fd04fec8a9940f5eba48d18c6369f432177b22
duyvo689 <vothanhduy689@gmail.com>
2022-09-26 19:02:35 +0700

    update about

--- lib/pages/profile_user/components/about.dart
+++ lib/pages/profile_user/components/about.dart
@@ -11,28 +11,30 @@ class AboutProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
         flex: 1,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: FutureBuilder<User>(
-              future: ApiUserServices().fetchUserById(userId),
-              builder: (context, snapshot) {
-                if ((snapshot.hasError) || (!snapshot.hasData))
-                  // ignore: curly_braces_in_flow_control_structures
-                  return const CircleLoading();
-                User? user = snapshot.data;
-                return user != null && user.descriptions != ''
-                    ? Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 10),
-                          Text(
-                            user.descriptions.toString(),
-                            style: AppStyles.about_profile_title_style,
-                          ),
-                        ],
-                      )
-                    : const NoContentProfile(title: "No about!");
-              }),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: FutureBuilder<User>(
+                future: ApiUserServices().fetchUserById(userId),
+                builder: (context, snapshot) {
+                  if ((snapshot.hasError) || (!snapshot.hasData))
+                    // ignore: curly_braces_in_flow_control_structures
+                    return const CircleLoading();
+                  User? user = snapshot.data;
+                  return user != null && user.descriptions != ''
+                      ? Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 10),
+                            Text(
+                              user.descriptions.toString(),
+                              style: AppStyles.about_profile_title_style,
+                            ),
+                          ],
+                        )
+                      : const NoContentProfile(title: "No about!");
+                }),
+          ),
         ));
   }
 }
