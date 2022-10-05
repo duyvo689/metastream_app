93de7722d8859de5c9cef92c9b4afee7e07a53b5
duyvo689 <vothanhduy689@gmail.com>
2022-10-05 11:09:17 +0700

    upadte

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,19 @@
 import 'package:app_metastream/funtions/funtions.dart';
 import 'package:app_metastream/pages/pages.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => UserInfo()),
-          ChangeNotifierProvider(create: (_) => GameListProvider()),
-          ChangeNotifierProvider(create: (_) => UserList()),
-          ChangeNotifierProvider(create: (_) => LiveStreamingProvider()),
-        ],
-        child:
-            DevicePreview(enabled: false, builder: (context) => const MyApp()),
-      ),
-    );
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        useInheritedMediaQuery: true,
-        locale: DevicePreview.locale(context),
-        builder: DevicePreview.appBuilder,
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          brightness: Brightness.dark,
-        ),
-        home: SplashPage());
-  }
-}
-
 // void main() => runApp(
-//       MultiProvider(providers: [
-//         ChangeNotifierProvider(create: (_) => UserInfo()),
-//         ChangeNotifierProvider(create: (_) => GameListProvider()),
-//         ChangeNotifierProvider(create: (_) => UserList()),
-//       ], child: const MyApp()),
+//       MultiProvider(
+//         providers: [
+//           ChangeNotifierProvider(create: (_) => UserInfo()),
+//           ChangeNotifierProvider(create: (_) => GameListProvider()),
+//           ChangeNotifierProvider(create: (_) => UserList()),
+//           ChangeNotifierProvider(create: (_) => LiveStreamingProvider()),
+//         ],
+//         child:
+//             DevicePreview(enabled: false, builder: (context) => const MyApp()),
+//       ),
 //     );
 
 // class MyApp extends StatelessWidget {
@@ -49,6 +22,9 @@ class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
+//         useInheritedMediaQuery: true,
+//         locale: DevicePreview.locale(context),
+//         builder: DevicePreview.appBuilder,
 //         debugShowCheckedModeBanner: false,
 //         title: 'Flutter Demo',
 //         theme: ThemeData(
@@ -57,3 +33,27 @@ class MyApp extends StatelessWidget {
 //         home: SplashPage());
 //   }
 // }
+
+void main() => runApp(
+      MultiProvider(providers: [
+        ChangeNotifierProvider(create: (_) => UserInfo()),
+        ChangeNotifierProvider(create: (_) => GameListProvider()),
+        ChangeNotifierProvider(create: (_) => UserList()),
+        ChangeNotifierProvider(create: (_) => LiveStreamingProvider()),
+      ], child: const MyApp()),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        home: SplashPage());
+  }
+}
